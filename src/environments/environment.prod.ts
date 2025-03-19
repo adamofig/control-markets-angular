5e6abc50b6aa943ffba7ad0b48170f7cb3c10b55
adamo jordan <adamo.figueroa@gmail.com>
2025-03-19 15:52:50 -0600

    adding variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,29 @@
+export const environment = {
+  projectName: 'Niche-Market',
+  version: '0.0.14',
+  envName: 'DEV',
+  production: false,
+
+  clientId: '',
+  androidClientId: '',
+  iosClientId: '',
+  backendNodeUrl: 'http://localhost:8080',
+  // backendNodeUrl: 'https://niche-market-905545672221.us-central1.run.app',
+  backendPythonUrl: 'http://localhost:8000',
+  // backendPythonUrl: 'https://python-server-905545672221.us-central1.run.app',
+
+  mobile: {
+    appleAppId: '',
+    appleRedirectURI: '',
+    androidClientId: '',
+    iosClientId: '',
+  },
+  firebase: {
+    apiKey: 'AIzaSyAQkpxs0id--VjwHQ4LiNEEZrgpvf40IqI',
+    authDomain: 'niche-market-dev.firebaseapp.com',
+    projectId: 'niche-market-dev',
+    storageBucket: 'niche-market-dev.firebasestorage.app',
+    messagingSenderId: '905545672221',
+    appId: '1:905545672221:web:684727e6a1c0d99bd508f4',
+  },
+};
