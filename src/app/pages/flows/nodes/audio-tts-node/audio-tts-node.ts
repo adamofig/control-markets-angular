33adbd9e52d465c6281e22dd2f975c3833775015
adamo jordan <adamo.figueroa@gmail.com>
2025-09-17 00:42:34 -0600

    solving issues

--- src/app/pages/flows/nodes/audio-tts-node/audio-tts-node.ts
+++ src/app/pages/flows/nodes/audio-tts-node/audio-tts-node.ts
@@ -1,13 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
 import { BaseFlowNode } from '../base-flow-node';
 import { CustomOutcomeNode } from '../outcome-node/outcome-node.component';
 import { TTSPlayground } from '@dataclouder/ngx-vertex';
 import { FlowSignalNodeStateService } from '../../services/flow-signal-node-state.service';
 import { IAssetNodeData } from '../../models/nodes.model';
+import { Vflow } from 'ngx-vflow';
+import { TextareaModule } from 'primeng/textarea';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-audio-tts-node-component',
-  imports: [TTSPlayground],
+  imports: [Vflow, TTSPlayground, TextareaModule, FormsModule, ButtonModule],
   templateUrl: './audio-tts-node.html',
   styleUrl: './audio-tts-node.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,6 +20,22 @@ export class AudioTTsNodeComponent extends BaseFlowNode<CustomOutcomeNode> {
   public flowSignalNodeStateService = inject(FlowSignalNodeStateService);
 
   public storagePath = 'flows/audios/' + this.flowSignalNodeStateService.flow()?.id;
+
+  public agentSource = signal<any>(null);
+
+  public value = '';
+
+  constructor() {
+    super();
+    effect(() => {
+      const agentSource = this.flowSignalNodeStateService.edges().find(edge => edge.target === this.node()?.id);
+      if (agentSource) {
+        console.log('Se encontro un node. de agente. ', agentSource);
+        this.agentSource.set(agentSource);
+      }
+    });
+  }
+
   onTtsGenerated(event: any) {
     console.log('tts generated', event);
 
@@ -30,5 +50,10 @@ export class AudioTTsNodeComponent extends BaseFlowNode<CustomOutcomeNode> {
     };
     this.flowSignalNodeStateService.addAssetNode(assetNodeData, this.node()?.id);
     // A ver que hago con el audio?
+  }
+
+  public generateTTS() {
+    // this.flowSignalNodeStateService.generateTTS(this.value);
+    alert('generateTTS');
   }
 }
