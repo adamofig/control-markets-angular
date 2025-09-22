5dd61f5b3add203dc137ec7f526e791629bd3953
adamo jordan <adamo.figueroa@gmail.com>
2025-09-21 23:45:09 -0600

    adding forms

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -3,21 +3,37 @@ import { IonicModule } from '@ionic/angular';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PlanType, PermissionType, RolType } from '../../dc-user-module/user.class';
 
-import { IUser, ProfileFormComponent } from '@dataclouder/ngx-users';
-import { TOAST_ALERTS_TOKEN } from '@dataclouder/ngx-core';
+import { IUser, ProfileFormComponent, ProfileSettingsFormComponent } from '@dataclouder/ngx-users';
+import { AudioSpeed, TOAST_ALERTS_TOKEN } from '@dataclouder/ngx-core';
 import { AppUserService } from 'src/app/services/app-user.service';
+import { CardModule } from 'primeng/card';
+import { DCConversationUserChatSettingsComponent } from '@dataclouder/ngx-agent-cards';
+
+export const BaseLanguagesOptions = [
+  { name: 'Español 🇪🇦 🇲🇽 ', code: 'es' },
+  { name: 'Inglés 🇺🇸 🇬🇧 ', code: 'en' },
+];
+
+export const AudioSpeedOptions = [
+  { name: 'Muy Lento', code: AudioSpeed.VerySlow },
+  { name: 'Lento', code: AudioSpeed.Slow },
+  { name: 'Normal', code: AudioSpeed.Regular },
+  { name: 'Rápida', code: AudioSpeed.Fast },
+];
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
   standalone: true,
-  imports: [IonicModule, ReactiveFormsModule, ProfileFormComponent],
+  imports: [IonicModule, ReactiveFormsModule, ProfileFormComponent, ProfileSettingsFormComponent, CardModule, DCConversationUserChatSettingsComponent],
 })
 export class ProfileComponent implements OnInit {
   private fb = inject(FormBuilder);
   public userService = inject(AppUserService);
   private toastService = inject(TOAST_ALERTS_TOKEN);
+  public hiddenFields = { targetLanguage: true };
+  public audioSpeedOptions = AudioSpeedOptions;
 
   public userForm = this.fb.group({
     firstname: [<string>'', Validators.required],
@@ -27,6 +43,15 @@ export class ProfileComponent implements OnInit {
     username: [<string>''],
     gender: [<string>''],
     emotionalName: [<string>''],
+  });
+  public baseLanguagesOptions = BaseLanguagesOptions;
+
+  public settingsForm = this.fb.group({
+    enableNotifications: [false],
+    audioSpeed: [1],
+    baseLanguage: [''],
+    targetLanguage: [''],
+    // conversation will be added in child
   });
 
   profileForm = this.fb.group({
